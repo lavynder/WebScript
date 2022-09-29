9bd47c3dcab61e41f72d0ab515d93b9363d8f91b
LF <leo.fanseda@ontariotechu.net>
2022-09-29 14:22:24 -0400

    Update LabActivity1.js
    added additional comments

--- Labs/LabActivity1.js
+++ Labs/LabActivity1.js
@@ -11,40 +11,52 @@ let computerSelection = Math.random()
 console.log(userSelection)
 console.log(computerSelection)
 
-// USING if STATEMENTS TO DETERMINE THE OUTCOME OF THE GAME
-// computerSelection CHOOSES PAPER
+// USES NESTED if STATEMENTS TO DETERMINE THE OUTCOME OF THE GAME
+// FIRST THE PROGRAM DETERMINES computerSelections CHOICE, 
+// THEN CHOOSES THE APPROPRIATE OUTCOME DEPENDING ON THE USER'S CHOICE
+
+// computerSelection IS LESS THAN 0.34, SO COMPUTER CHOOSES PAPER
 if(computerSelection <= 0.34) {
     if (userSelection == 'ROCK') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('Computer Wins')
     }
     else if (userSelection == 'PAPER') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('It\'s a tie')
     }
     else if(userSelection == 'SCISSORS') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('User Wins')
     }
 }
-// computerSelection CHOOSES SCISSORS
+// computerSelection IS BETWEEN 0.35 AND 0.67, SO COMPUTER CHOOSES SCISSORS
 else if (computerSelection >= 0.35 && computerSelection <= 0.67){
     if (userSelection == 'ROCK') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('User Wins')
     }
     else if (userSelection == 'PAPER') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('Computer Wins')
     }
     else if(userSelection == 'SCISSORS') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('It\'s a tie')
     }
 }
-// computerSelection CHOOSES ROCK
+// computerSelection IS GREATER THAN 0.68, SO COMPUTER CHOOSES ROCK
 else if (computerSelection >= 0.68) {
     if (userSelection == 'ROCK') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('It\'s a tie')
     }
     else if (userSelection == 'PAPER') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('User Wins')
     }
     else if(userSelection == 'SCISSORS') {
+        // DISPLAYS OUTCOME OF THE GAME
         console.log('Computer Wins')
     }
 }
